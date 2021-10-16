40da455f413a0dcb3fb2834a4f443d1e5ca67984
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-17 01:32:54 +0330

    Get all Twitter env variables

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 # take environment variables from .env.
 load_dotenv()
 
-# get environment variable BEARER_TOKEN
+# get environment variables for Twitter API
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+token_secret = os.environ.get("TOKEN_SECRET")
 bearer_token = os.environ.get("BEARER_TOKEN")
 
 # use curl to get tweet data
