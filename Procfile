1312e5b36c01fe805f6bcd63e88058021ef1981c
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-17 16:15:09 +0330

    Heroku Deployment Problem: Put all code in server.py

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn src.server:app
-web: gunicorn src.bot:app
--- src/server.py
+++ src/server.py
@@ -1,10 +1,26 @@
 import os
+import tweepy
 from flask import Flask
 from dotenv import load_dotenv
-import bot
+
 
 # take environment variables from .env.
 load_dotenv()
+
+# get environment variables for Twitter API
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+bearer_token = os.environ.get("BEARER_TOKEN")
+
+def get_tweet_text(tweetId):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    status = api.get_status(id=tweetId)
+    return status.user.name, status.text
+
 
 app = Flask(__name__)
 
@@ -12,7 +28,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     #bot.tweet_hello_world()
-    username, text = bot.get_tweet_text(20)
+    username, text = get_tweet_text(20)
     return f"{username}: {text}"
 
 if __name__ == "__main__":
