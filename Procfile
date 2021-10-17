045350e8d7c0e1ba83adc3367ed697821ff43ac8
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-17 15:02:26 +0330

    Test Procfile with hello.py at root

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn src.server:app
+web: gunicorn hello:app
---
+++ hello.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+# take environment variables from .env.
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello World from Flask!"
+
+
+app.run(host="localhost", port=os.environ.get("PORT") or 3456)
