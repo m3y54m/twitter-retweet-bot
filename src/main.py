aeda2b950c92a7e3ed59df7131708273390d18e0
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-17 04:07:00 +0330

    Print tweets of timeline in file

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-    
+
+with open('timeline.txt', 'w') as f:
+    for tweet in public_tweets:
+        f.write(tweet.text + "\r\n")
