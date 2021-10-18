f3341634c455728bf950f5baa5c1beb2d42646ad
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-18 15:43:48 +0330

    Heroku Test: Moved functions to bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,7 @@
 import os
 import tweepy
 from dotenv import load_dotenv
+
 # take environment variables from .env.
 load_dotenv()
 
@@ -12,33 +13,56 @@ access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 bearer_token = os.environ.get("BEARER_TOKEN")
 
 
-# os.system(f'curl -X GET -H "Authorization: Bearer {bearer_token}" "https://api.twitter.com/2/tweets/20"')
-# print("")
-
-def tweet_hello_world():
+def twitter_api_authenticate():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    api.update_status(status="Hello World from Bot!")
 
-def get_tweet_text(tweetId):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    status = api.get_status(id=tweetId)
-    return status.user.name, status.text
+    try:
+        api.verify_credentials()
+    except Exception as error:
+        print(
+            f"\n[ SimJowBot ] An error occurred while attempting to authenticate with the twitter API. Reason:\n{error}"
+        )
+        return None
+    else:
+        return api
 
-def get_timeline():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    public_tweets = api.home_timeline()
-    timeline_list=[]
-    for tweet in public_tweets:
-        timeline_list.append(tweet.text)
-    return timeline_list
 
-    
+def get_tweet(twitterApi, tweetId):
+    userName = None
+    tweetText = None
+
+    if twitterApi:
+        try:
+            status = twitterApi.get_status(id=tweetId)
+        except Exception as error:
+            print(
+                f"\n[ SimJowBot ] An error occurred while attempting to get the twitter status with id={tweetId}. Reason:\n{error}"
+            )
+        else:
+            userName = status.user.name
+            tweetText = status.text
+
+    return userName, tweetText
+
+
+def post_tweet(twitterApi, tweetText):
+    success = False
+
+    if twitterApi:
+        try:
+            twitterApi.update_status(status=tweetText)
+        except Exception as error:
+            print(
+                f"\n[ SimJowBot ] An error occurred while attempting to update the twitter status. Reason:\n{error}"
+            )
+        else:
+            success = True
+
+    return success
+
+
 # public_tweets = api.home_timeline()
 
 # with open('timeline.txt', 'w') as f:
@@ -50,5 +74,4 @@ if __name__ == "__main__":
     # username, text = get_tweet_text(20)
     # print(f"{username}: {text}")
 
-    #print(get_timeline())
-
+    # print(get_timeline())
--- src/server.py
+++ src/server.py
@@ -1,70 +1,13 @@
 import os
 import time
 from datetime import datetime
-import tweepy
+import bot
 from flask import Flask
 from dotenv import load_dotenv
 
 
 # take environment variables from .env.
 load_dotenv()
-
-# get environment variables for Twitter API
-consumer_key = os.environ.get("CONSUMER_KEY")
-consumer_secret = os.environ.get("CONSUMER_SECRET")
-access_token = os.environ.get("ACCESS_TOKEN")
-access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
-bearer_token = os.environ.get("BEARER_TOKEN")
-
-
-def twitter_api_authenticate():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-
-    try:
-        api.verify_credentials()
-    except Exception as error:
-        print(
-            f"\n[ SimJowBot ] An error occurred while attempting to authenticate with the twitter API. Reason:\n{error}"
-        )
-        return None
-    else:
-        return api
-
-
-def get_tweet(twitterApi, tweetId):
-    userName = None
-    tweetText = None
-
-    if twitterApi:
-        try:
-            status = twitterApi.get_status(id=tweetId)
-        except Exception as error:
-            print(
-                f"\n[ SimJowBot ] An error occurred while attempting to get the twitter status with id={tweetId}. Reason:\n{error}"
-            )
-        else:
-            userName = status.user.name
-            tweetText = status.text
-
-    return userName, tweetText
-
-
-def post_tweet(twitterApi, tweetText):
-    success = False
-
-    if twitterApi:
-        try:
-            twitterApi.update_status(status=tweetText)
-        except Exception as error:
-            print(
-                f"\n[ SimJowBot ] An error occurred while attempting to update the twitter status. Reason:\n{error}"
-            )
-        else:
-            success = True
-
-    return success
 
 
 def get_date_time():
@@ -79,26 +22,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    twitterApi = twitter_api_authenticate()
-    userName, text = get_tweet(twitterApi, 20)
+    twitterApi = bot.twitter_api_authenticate()
+    userName, text = bot.get_tweet(twitterApi, 20)
     return f"{userName}: {text}"
 
 
 if __name__ == "__main__":
-    # app.debug = False
-    # app.run(host="localhost", port=os.environ.get("PORT") or 3456)
+    app.debug = False
+    app.run(host="localhost", port=os.environ.get("PORT") or 3456)
 
-    # tweet interval in seconds
-    interval = 60
+    # # tweet interval in seconds
+    # interval = 60
 
-    while True:
+    # while True:
 
-        twitterApi = twitter_api_authenticate()
-        text = (
-            f"Consecutive Test Tweets at {interval} Seconds Interval:\n"
-            + get_date_time()
-        )
-        
-        post_tweet(twitterApi, text)
-        # wait for the interval
-        time.sleep(interval)
+    #     twitterApi = bot.twitter_api_authenticate()
+    #     text = (
+    #         f"Consecutive Test Tweets at {interval} Seconds Interval:\n"
+    #         + get_date_time()
+    #     )
+
+    #     bot.post_tweet(twitterApi, text)
+    #     # wait for the interval
+    #     time.sleep(interval)
