514d60f43464e8349862903358b76e1de56dbebe
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-08-31 18:56:48 +0430

    Define a function to generate the Stream Rules list

--- src/main.py
+++ src/main.py
@@ -6,9 +6,61 @@ from server import start_server_thread
 SRC_PATH = pathlib.Path(__file__).parent.resolve()
 TRACK_JSON_PATH = SRC_PATH.joinpath("track.json")
 
+MAX_RULES_COUNT = 25
+
+def create_rules_list(keywordsList, ruleMaxLength):
+    keywordsListSize = len(keywordsList)
+    ruleInitial = f"lang:fa -is:retweet -is:reply -from:{bot.bot_username} -retweets_of:{bot.bot_username} ("
+    rule = ruleInitial
+    ruleSize = len(rule)
+    rulesList = []
+    portionsCount = 0
+
+    if ruleSize < ruleMaxLength:
+        # Iterate over keywords until the one before the last item
+        for i in range(keywordsListSize - 1):
+
+            # Get two consecutive items from list
+            currentKeywordString = f'"{keywordsList[i]}" OR '
+            currentKeywordStringLast = f'"{keywordsList[i]}")'
+            nextKeywordStringLast = f'"{keywordsList[i+1]}")'
+
+            ck = len(currentKeywordString)
+            ckl = len(currentKeywordStringLast)
+            nkl = len(nextKeywordStringLast)
+
+            ruleSize = len(rule)
+
+            if i < keywordsListSize - 2:
+                if ruleSize + ck + nkl < ruleMaxLength:
+                    rule += currentKeywordString
+                    # Continue until portion is full
+                elif ruleSize + ck + nkl == ruleMaxLength:
+                    rule += currentKeywordString + nextKeywordStringLast
+                    rulesList.append(rule)
+                    portionsCount += 1
+                    rule = ruleInitial
+                elif (
+                    ruleSize + ck + nkl > ruleMaxLength
+                    and ruleSize + ckl <= ruleMaxLength
+                ):
+                    rule += currentKeywordStringLast
+                    rulesList.append(rule)
+                    portionsCount += 1
+                    rule = ruleInitial
+            else:  # if (i == keywordsListSize - 2)
+                if ruleSize + ck + nkl <= ruleMaxLength:
+                    rule += currentKeywordString + nextKeywordStringLast
+                    rulesList.append(rule)
+                    portionsCount += 1
+                    # Finish
+    
+    return rulesList
+
+
 if __name__ == "__main__":
 
-    with open(TRACK_JSON_PATH, "r") as trackJson:
+    with open(TRACK_JSON_PATH, "r", encoding="utf-8") as trackJson:
 
         trackDic = json.load(trackJson)
 
@@ -24,46 +76,50 @@ if __name__ == "__main__":
         # Add mentions to track list
         # trackList.extend(mentionsList)
 
-        hashtagsString = ' '.join(f'"{item}"' for item in hashtagsList)
-        
+        rulesList = create_rules_list(trackList, 512)
+        rulesCount = len(rulesList)
 
-        print(hashtagsString)
-        print(len(hashtagsString))
+        if (rulesCount > MAX_RULES_COUNT):
+            print(
+                f"\n[SimJowBot] [{bot.get_datetime()}] [WARNING] Number of required rules for the bot ({rulesCount}) is more than allowed ({MAX_RULES_COUNT})"
+            )
 
         # keep the bot running in replit
         start_server_thread()
 
-        # # create a tweepy Stream object for real time filtering of latest posted tweets
-        # streamClient = bot.SimJowStream(bot.bearer_token, True)
+        # create a tweepy Stream object for real time filtering of latest posted tweets
+        streamClient = bot.SimJowStream(bot.bearer_token, True)
 
-        # # Get a list of all rules
-        # rules = streamClient.get_rules().data
-        # ids = []
-        # if (rules):
-        #     # Create the list of rule ids
-        #     for rule in rules:
-        #         ids.append(rule.id)
-        #     # Delete all rules
-        #     streamClient.delete_rules(ids)
+        # Get a list of all rules registered on the bot's Twitter app
+        rules = streamClient.get_rules().data
+        ids = []
+        if (rules):
+            # Create the list of rule ids
+            for rule in rules:
+                ids.append(rule.id)
+            # Delete all rules
+            streamClient.delete_rules(ids)
 
-        # streamRuleString = f'"ایران در" lang:fa -is:retweet -is:reply -from:SimJow -retweets_of:SimJow'
-        # streamRule = bot.tweepy.StreamRule(streamRuleString)
-        # streamClient.add_rules(streamRule)
+        
+        # Register the rules to the bot
+        for i in range(MAX_RULES_COUNT):
+            streamRule = bot.tweepy.StreamRule(rulesList[i])
+            streamClient.add_rules(streamRule)
 
-        # # To keep the bot running even if there is an error
-        # while True:
+        # To keep the bot running even if there is an error
+        while True:
 
-        #     print(
-        #         f"\n[SimJowBot] [{bot.get_datetime()}] [INFO] Stream monitoring has started."
-        #     )
+            print(
+                f"\n[SimJowBot] [{bot.get_datetime()}] [INFO] Stream monitoring has started."
+            )
 
-        #     try:
-        #         # start filtering the twitter stream in a loop
-        #         streamClient.filter(
-        #             expansions=["author_id"],
-        #             user_fields=["username"],
-        #         )
-        #     except Exception as error:
-        #         print(
-        #             f"\n[SimJowBot] [{bot.get_datetime()}] [ERROR] Something is wrong with tweets stream. Reason:\n{error}"
-        #         )
+            try:
+                # start filtering the twitter stream in a loop
+                streamClient.filter(
+                    expansions=["author_id"],
+                    user_fields=["username"],
+                )
+            except Exception as error:
+                print(
+                    f"\n[SimJowBot] [{bot.get_datetime()}] [ERROR] Something is wrong with tweets stream. Reason:\n{error}"
+                )
